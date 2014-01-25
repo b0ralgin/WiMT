7b0ea551560ea27470abb95d72480123e43cf582
Enso <Enso@ip-192-168-0-102.is74.loc>
2014-01-25 23:30:12 +0600

    Initial Commit

---
+++ WiMTTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WiMTTest
+//
+//  Created by Enso on 25.01.14.
+//  Copyright (c) 2014 AppBit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
