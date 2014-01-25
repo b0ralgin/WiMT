f4252624204a53b6d459794391bc7caeb4a49d42
Akira Yamaoka <akira@ip-192-168-0-106.is74.loc>
2014-01-25 23:16:04 +0600

    First

---
+++ WiMT/WiMT/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WiMT
+//
+//  Created by Akira Yamaoka on 25.01.14.
+//  Copyright (c) 2014 AppBit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WiMT/WiMT/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  WiMT
+//
+
+//  Copyright (c) 2014 AppBit. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
