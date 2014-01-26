11fc0c947d98ff362cf6f449a63ab07f5aa6fea5
Enso <Enso@ip-192-168-0-101.is74.loc>
2014-01-26 11:19:52 +0600

    add obj

--- WiMTTest/LivingScene.m
+++ WiMTTest/LivingScene.m
@@ -26,17 +26,17 @@
 - (void)initObjects {
        [self addObject:@"HangDark" WithObjectType:ROOM_OBJECT OnPos:CGPointMake(100, 108) Dynamic:NO];
     [self addObject:@"ClockDark" WithObjectType:ROOM_OBJECT OnPos:CGPointMake(400, 500) Dynamic:NO];
-    Trap* Waver = [self addTrap:@"WaverDark" Light:@"WaverLight" OnPos:CGPointMake(408, 294) Dynamic:NO];
+    Trap* waver = [self addTrap:@"WaverDark" Light:@"WaverLight" OnPos:CGPointMake(408, 294) Dynamic:NO];
     [self addObject:@"SofaDark" WithObjectType:ROOM_OBJECT OnPos:CGPointMake(700, 120) Dynamic:NO];
     [self addObject:@"PaintingDark" WithObjectType:ROOM_OBJECT OnPos:CGPointMake(800, 500) Dynamic:NO];
    // [self addObject:@"Flower" WithObjectType:ENEMY_OBJECT OnPos:CGPointMake(750, 108) Dynamic:YES];
     [self addObject:@"candy" WithObjectType:CANDY_OBJECT OnPos:CGPointMake(800, 108) Dynamic:NO];
     [self addObject:@"candy" WithObjectType:CANDY_OBJECT OnPos:CGPointMake(900, 108) Dynamic:NO];
     [self addObject:@"ArmchairDark" WithObjectType:ROOM_OBJECT OnPos:CGPointMake(1300, 108) Dynamic:NO];
-    [self addObject:@"Spikes" WithObjectType:ROOM_OBJECT OnPos:CGPointMake(1600, 100) Dynamic:NO];
+    Trap* spikes = [self addTrap:@"SpikesDark" Light:@"SpikesLight" OnPos:CGPointMake(1700, 100) Dynamic:NO];
     [self addObject:@"BedroomDoorClose" WithObjectType:DOOR_OBJECT OnPos:CGPointMake(2000, 120) Dynamic:NO];
     [self addObject:@"SpeakerDark" WithObjectType:BOX_OBJECT OnPos:CGPointMake(2200, 300) Dynamic:NO];
-    [self addObject:@"switch_off" WithObjectType:SWITCH_OBJECT OnPos:CGPointMake(2300, 500) Dynamic:NO];
+    [self addObject:@"switch_off" WithObjectType:SWITCH_OBJECT OnPos:CGPointMake(2500, 500) Dynamic:NO];
     //[self addObject:@"TV" WithObjectType:ENEMY_OBJECT OnPos:CGPointMake(1700, 108) Dynamic:YES];
     [self addObject:@"ChairDark" WithObjectType:BOX_OBJECT OnPos:CGPointMake(2700, 108) Dynamic:YES];
 }
