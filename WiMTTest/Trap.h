617f3c8f2c907758de4e4696af662ea710b1e569
Alexander Semenov <smn@ip-192-168-0-102.is74.loc>
2014-01-26 10:38:57 +0600

    qweqwe

---
+++ WiMTTest/Trap.h
@@ -0,0 +1,13 @@
+//
+//  Trap.h
+//  WiMTTest
+//
+//  Created by Alexander Semenov on 26.01.14.
+//  Copyright (c) 2014 AppBit. All rights reserved.
+//
+
+#import "GameObject.h"
+
+@interface Trap : GameObject
+
+@end
---
+++ WiMTTest/Trap.m
@@ -0,0 +1,13 @@
+//
+//  Trap.m
+//  WiMTTest
+//
+//  Created by Alexander Semenov on 26.01.14.
+//  Copyright (c) 2014 AppBit. All rights reserved.
+//
+
+#import "Trap.h"
+
+@implementation Trap
+
+@end
